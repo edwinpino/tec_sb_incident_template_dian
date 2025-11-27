b6418d0e61e9b75f8297d04e9a7c8c4a875f2fcf
epinoc <epinoc@dian.gov.co>
2025-11-27 10:12:10 -0500

    AB#1002-chore(estructura): Añadir estructura Maven inicial (src)

---
+++ src/main/java/gov/dian/devops/IncidentApplication.java
@@ -0,0 +1,23 @@
+package gov.dian.devops;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * Punto de entrada de la aplicación.
+ * 
+ */
+
+@SpringBootApplication
+
+public class IncidentApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IncidentApplication.class, args);
+
+    }
+
+}
